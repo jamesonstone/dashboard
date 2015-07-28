127cd012fcd529b68c8a89883324c466962a23ee
J.Stone <jamesonstone@iheartmedia.com>
2015-07-28 14:12:21 -0400

    wip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,21 @@ class PagesController < ApplicationController
 
 
   def root
-  	url = 
-  	'http://1111096:********@ios-buildmaster.ihrdev.com/job/UI-Automation/lastBuild/api/json'
+  	ios_flagship_url = 
+  	'http://1111096:****@ios-buildmaster.ihrdev.com/job/UI-Automation/lastBuild/api/json'
 
-  	response = HTTParty.get(url, :verify => false)
-  	@result = response['result']
-  	@display_name = response['fullDisplayName']
-  	@current_time = Time.now.strftime("(last checked %m/%d/%Y | %H:%M)")
+  	ios_flagship_response = HTTParty.get(ios_flagship_url, :verify => false)
+  	@ios_flagship_result = ios_flagship_response['result']
+  	@ios_flagship_display_name = ios_flagship_response['fullDisplayName']
+  	@ios_flagship_current_time = Time.now.strftime("(last checked %m/%d/%Y | %H:%M)")
 
+  	android_flagship_url = 
+  	'http://1111096:*****@ios-buildmaster.ihrdev.com/job/UI-Automation/lastBuild/api/json'
 
+  	android_flagship_response = HTTParty.get(android_flagship_url, :verify => false)
+  	@ios_flagship_result = android_flagship_response['result']
+  	@ios_flagship_display_name = android_flagship_response['fullDisplayName']
+  	@ios_flagship_current_time = Time.now.strftime("(last checked %m/%d/%Y | %H:%M)")
 
 
 
