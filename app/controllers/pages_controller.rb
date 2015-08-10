86108ec807da5f9cfe96fd78b36c2b10a76dbc04
Jameson <jameson@voxy.com>
2015-08-09 22:38:24 -0400

    not sure actually

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,24 +2,27 @@ class PagesController < ApplicationController
 
 
   def root
-  	ios_flagship_url = 
-  	'http://1111096:***@ios-buildmaster.ihrdev.com/job/ios-flagship/lastBuild/api/json'
+    ios_flagship_url =
+        'http://1111096:***@ios-buildmaster.ihrdev.com/job/ios-flagship/lastBuild/api/json'
 
-  	ios_flagship_response = HTTParty.get(ios_flagship_url, :verify => false)
-  	@ios_flagship_result = ios_flagship_response['result']
-  	@ios_flagship_display_name = ios_flagship_response['fullDisplayName']
-  	@ios_flagship_current_time = Time.now.strftime("(last checked %m/%d/%Y | %H:%M)")
+    ios_flagship_response = HTTParty.get(ios_flagship_url, :verify => false)
+    @ios_flagship_result = ios_flagship_response['result']
+    @ios_flagship_display_name = ios_flagship_response['fullDisplayName']
+    @ios_flagship_current_time = Time.now.strftime("(last checked %m/%d/%Y | %H:%M)")
 
-  	android_flagship_url = 
-  	'http://1111096:***@ios-buildmaster.ihrdev.com/job/ios-flagship/lastBuild/api/json'
+    android_flagship_url =
+        'http://1111096:***@ios-buildmaster.ihrdev.com/job/ios-flagship/lastBuild/api/json'
 
-  	android_flagship_response = HTTParty.get(android_flagship_url, :verify => false)
-  	#@ios_flagship_result = android_flagship_response['result']
-  	@ios_flagship_display_name = android_flagship_response['fullDisplayName']
-  	@ios_flagship_current_time = Time.now.strftime("(last checked %m/%d/%Y | %H:%M)")
+    android_flagship_response = HTTParty.get(android_flagship_url, :verify => false)
+    #@ios_flagship_result = android_flagship_response['result']
+    @ios_flagship_display_name = android_flagship_response['fullDisplayName']
+    @ios_flagship_current_time = Time.now.strftime("(last checked %m/%d/%Y | %H:%M)")
 
 
+  end
 
+  def getCurrentTime
+    return Time.now.strftime("(Submitted: %m/%d/%Y | %H:%M)")
   end
 
 end
