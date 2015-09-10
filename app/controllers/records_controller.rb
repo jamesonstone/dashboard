00174fd8fe55182522799e1b2c5fb851389ddc3f
J.Stone <jamesonstone@iheartmedia.com>
2015-09-10 11:31:18 -0400

    clean up project adding

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,14 @@
 class RecordsController < ApplicationController
 
+  @@project_names = [
+      'Android Flagship',
+      'iOS Flagship',
+      'iOS Auto',
+      'Android Auto',
+      'Web',
+      'Local'
+  ]
+
   def index
     @records = Record.all
   end
@@ -16,8 +25,28 @@ class RecordsController < ApplicationController
   end
 
   def create
+    #create a new record
     @record = Record.new(record_params)
+
+    #check to see if the name is a core projects
+    for name in @@project_names
+      if name != @record.project_name
+        @record.third_party = true
+      else
+        @record.third_party = false
+      end
+    end
+
+    #set the color based on status
+    if @record.status == "Fail"
+      @record.color = "red"
+    else
+      @record.color = "green"
+    end
+
+    #save the record
     @record.save
+    #redirect to single record view (show)
     redirect_to @record
   end
 
